eb5a81c6ab6760bdc9e02969d44e0ab72766ba62
Oseias Beu <oseiasbeu@outlook.com>
2018-05-04 22:03:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function amplia(div){    
+        console.log('Ampliou!');
+        div.style.transform = 'scale(2,2)';
+}
+
+function encolhe(div){
+    console.log('Encolheu!');
+    div.style.transform = 'scale(1,1)';
+}
